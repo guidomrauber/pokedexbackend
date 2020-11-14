000db2c47a87b1227b1db5ec9ef13b57b8449c78
guidorauber <guidomrauber@gmail.com>
2020-11-14 08:58:21 +0000

    mejorando aspectos

--- controllers/pokedex.controller.js
+++ controllers/pokedex.controller.js
@@ -1,5 +1,5 @@
 const db = require("../models"); // models path depend on your structure
-const Tutorial = db.tutorials;
+const Pokedex = db.tutorials;
 
 exports.create = (req, res) => {
   // Validate request
@@ -10,8 +10,8 @@ exports.create = (req, res) => {
     return;
   }
 
-  // Create a Tutorial
-  const tutorial = {
+  // Create a Pokedex
+  const pokedex = {
     order: req.body.order,
     name: req.body.name,
 	sampleImage: req.body.sampleImage,
@@ -19,15 +19,15 @@ exports.create = (req, res) => {
     habilidad: req.body.habilidad
   };
 
-  // Save Tutorial in the database
-  Tutorial.create(tutorial)
+  // Save Pokemon in the database
+  Pokedex.create(pokedex)
     .then(data => {
       res.send(data);
     })
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the Tutorial."
+          err.message || "Some error occurred while creating the Pokedex."
       });
     });
 };
@@ -36,14 +36,14 @@ exports.findAll = (req, res) => {
     const name = req.query.name;
     var condition = name ? { name: { [Op.like]: `%${name}%` } } : null;
   
-    Tutorial.findAll({ where: condition })
+    Pokedex.findAll({ where: condition })
       .then(data => {
         res.send(data);
       })
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while retrieving tutorials."
+            err.message || "Some error occurred while retrieving pokedexs."
         });
       });
   };
@@ -51,80 +51,80 @@ exports.findAll = (req, res) => {
   exports.findOne = (req, res) => {
     const id = req.params.id;
   
-    Tutorial.findByPk(id)
+    Pokedex.findByPk(id)
       .then(data => {
         res.send(data);
       })
       .catch(err => {
         res.status(500).send({
-          message: "Error retrieving Tutorial with id=" + id
+          message: "Error retrieving Pokedex with id=" + id
         });
       });
   };
   exports.update = (req, res) => {
     const id = req.params.id;
   
-    Tutorial.update(req.body, {
+    Pokedex.update(req.body, {
       where: { id: id }
     })
       .then(num => {
         if (num == 1) {
           res.send({
-            message: "Tutorial was updated successfully."
+            message: "Pokedex was updated successfully."
           });
         } else {
           res.send({
-            message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`
+            message: `Cannot update Pokedex with id=${id}. Maybe Tutorial was not found or req.body is empty!`
           });
         }
       })
       .catch(err => {
         res.status(500).send({
-          message: "Error updating Tutorial with id=" + id
+          message: "Error updating Pokedex with id=" + id
         });
       });
   };
   exports.delete = (req, res) => {
     const id = req.params.id;
   
-    Tutorial.destroy({
+    Pokedex.destroy({
       where: { id: id }
     })
       .then(num => {
         if (num == 1) {
           res.send({
-            message: "Tutorial was deleted successfully!"
+            message: "Pokedex was deleted successfully!"
           });
         } else {
           res.send({
-            message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+            message: `Cannot delete Pokedex with id=${id}. Maybe Tutorial was not found!`
           });
         }
       })
       .catch(err => {
         res.status(500).send({
-          message: "Could not delete Tutorial with id=" + id
+          message: "Could not delete Pokedex with id=" + id
         });
       });
   };
   exports.deleteAll = (req, res) => {
-    Tutorial.destroy({
+    Pokedex.destroy({
       where: {},
       truncate: false
     })
       .then(nums => {
-        res.send({ message: `${nums} Tutorials were deleted successfully!` });
+        res.send({ message: `${nums} Pokedexs were deleted successfully!` });
       })
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while removing all tutorials."
+            err.message || "Some error occurred while removing all pokedexs."
         });
       });
   };
   exports.findAllPublished = (req, res) => {
 
-    Tutorial.findAll({ where: { published: true } })
+    Pokedex.findAll({ where: { published: true } })
     
     .then(data => {
     
@@ -138,7 +138,7 @@ exports.findAll = (req, res) => {
     
     message:
     
-    err.message || "Some error occurred while retrieving tutorials."
+    err.message || "Some error occurred while retrieving pokedexs."
     
     });
     
